f264da6a66e3a2a6402a85400948a5793aba72c1
arjunvenkat180003 <56486656+arjunvenkat180003@users.noreply.github.com>
2024-04-12 17:19:58 -0500

    Add files via upload

---
+++ Interaction.java
@@ -0,0 +1,11 @@
+
+public class Interaction {
+    String userId;
+    Post post;
+
+    Interaction(String userId, Post post)
+    {
+        this.userId = userId;
+        this.post = post;
+    }
+}
---
+++ Post.java
@@ -0,0 +1,13 @@
+import java.util.List;
+public class Post {
+    String postId;
+    String location;
+    List<String> keywords;
+
+    Post(String pId, String loc, List<String> kWords)
+    {
+        postId = pId;
+        location = loc;
+        kWords = keywords;
+    }
+}
